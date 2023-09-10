6a1a3cdc8820fea1074ec4ee3e47f42a31a6de1d
irarayzelji2002 <irarayzelji@gmail.com>
2023-09-10 18:00:11 +0800

    moved HW1 to folder and added MP1 folder

---
+++ ITELEC1C_MP1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITELEC1C_MP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
